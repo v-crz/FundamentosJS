9b6a5fa3a36c160bdde7b9083ae030e92a422dff
viry <vbatten10@hotmail.com>
2022-07-18 16:35:50 -0500

    Variables Const

---
+++ js/02.js
@@ -0,0 +1,8 @@
+// Variables con const
+// No se pueden reasignar
+// No pueden iniciar sin valor
+const CLIENTE = 'Juan';
+const PRECIO_DESCUENTO = 220;
+
+console.log(CLIENTE);
+console.log(PRECIO_DESCUENTO);
