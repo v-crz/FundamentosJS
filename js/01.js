03597fc3541847450b8fd58f2475be2c477d51c8
viry <vbatten10@hotmail.com>
2022-07-18 16:19:44 -0500

    Variables let

---
+++ js/01.js
@@ -0,0 +1,10 @@
+// Variables con let
+// se pueden reasignar y se pueden iniciar sin un valor
+let cliente = 'Juan';
+let precioDescuento = 220;
+
+console.log(cliente);
+console.log(precioDescuento);
+
+cliente = 20;
+console.log(cliente);
