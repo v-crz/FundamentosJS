d2bd94cf0d55d9921f36b3b0712d12bb82578ead
viry <vbatten10@hotmail.com>
2022-07-19 15:53:13 -0500

    Parámetros y parámetros por default

--- js/12.js
+++ js/12.js
@@ -1,8 +1,12 @@
 // Function declaration
 // Hoisting: primero se registran todas las funciones y después de ejecutan.
 
-function sumar() {
-    console.log( 2 + 2);
+// Parametro por default
+function sumar(numero = 0, numero2 = 0) {
+    console.log( numero + numero2 );
 }
 
+sumar(10, 20);
+sumar(100, 400);
+sumar(100);
 sumar();
