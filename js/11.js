1333875015a96f8f7363d48f2de6d009aec2f64e
viry <vbatten10@hotmail.com>
2022-07-19 15:31:52 -0500

    Iteradores de arreglos

---
+++ js/11.js
@@ -0,0 +1,16 @@
+// Iteradores en JS
+const tecnologias = ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js'];
+
+// forEach - Acceder a cada elemento del array
+// Listar y acceder a cada elemento del arreglo
+const arrayForEach = tecnologias.forEach(function(tech){
+    return tech;
+});
+
+// Permite crear un nuevo arreglo
+const arrayMap = tecnologias.map(function(tech){
+    return tech;
+});
+
+console.log(arrayForEach);
+console.log(arrayMap);
