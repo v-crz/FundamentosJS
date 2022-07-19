ff87edef0eaddc38ad841cdfec63242bb83f2fe1
viry <vbatten10@hotmail.com>
2022-07-19 15:59:40 -0500

    Funciones que retornan valores

--- js/12.js
+++ js/12.js
@@ -2,11 +2,35 @@
 // Hoisting: primero se registran todas las funciones y después de ejecutan.
 
 // Parametro por default
+// function sumar(numero = 0, numero2 = 0) {
+//     console.log( numero + numero2 );
+// }
+
+// sumar(10, 20);
+// sumar(100, 400);
+// sumar(100);
+// sumar();
+
+
+
+// Return array
+// function sumar(numero = 0, numero2 = 0) {
+//     return [numero + numero2, 'Hola mundo'];
+// }
+
+// const [resultado, holaMundo] = sumar(20, 30);
+// console.log(resultado);
+// console.log(holaMundo);
+
+
+// Return object
 function sumar(numero = 0, numero2 = 0) {
-    console.log( numero + numero2 );
+    return {
+        resultado: numero + numero2, 
+        mensaje: 'Hola mundo'
+    };
 }
 
-sumar(10, 20);
-sumar(100, 400);
-sumar(100);
-sumar();
+const {resultado, mensaje} = sumar(20, 30);
+console.log(resultado);
+console.log(mensaje);
