e5a1549f9251825b3bef0a99da3702a4b5f0a281
viry <vbatten10@hotmail.com>
2022-07-20 11:38:59 -0500

    Condicionales if

---
+++ js/17.js
@@ -0,0 +1,10 @@
+// Condicionales
+
+const disponible = 4000;
+const retirar = 200;
+
+if (disponible > retirar) {
+    console.log('Si puedes retirar');
+} else {
+    console.log('No puedes retirar');
+}
